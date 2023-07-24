c86bafb499e9d52d7a5aee64216bd69a6d737480
TrinhVanTung <tvtung07122001@gmail.com>
2023-07-24 22:17:53 +0700

    Add new branch

--- src/vn/trinhtung/Main.java
+++ src/vn/trinhtung/Main.java
@@ -2,10 +2,10 @@ package vn.trinhtung;
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("2");
-=======
 		System.out.println("1");
->>>>>>> 7b37802fa20df77cea7afde9885f30ab4c4498be
+		
+		int i = 10;
+		System.out.println(i);
 	}
 }
